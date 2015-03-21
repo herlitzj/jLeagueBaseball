e520cf3661f3c3faf376e29d25015723fa77b588
herlitzj <herlitzj@gmail.com>
2015-03-21 12:35:41 -0400

    Play mechanics and db mechanics functional

--- DB/mysql_test.rb
+++ DB/mysql_test.rb
@@ -2,22 +2,7 @@
 
 require 'mysql'
 
-
-=begin
-    con = Mysql.new 'localhost', 'root', 'waffles!'
-    puts con.get_server_info
-    rs = con.query 'SELECT VERSION()'
-    puts rs.fetch_row    
-    
-rescue Mysql::Error => e
-    puts e.errno
-    puts e.error
-    
-ensure
-    con.close if con
-=end
-
-def sql_querie(num, vs, play)
+def sql_query(num, vs, play)
 	if num == 1
 		begin
 		    con = Mysql.new 'localhost', 'root', 'waffles!', 'jLeague'
@@ -28,8 +13,6 @@ def sql_querie(num, vs, play)
 		    play = con.query("SELECT #{play} FROM bloomquist_willie")
 		    p_rows = play.num_rows
 		    
-		    #puts "There are #{n_rows} rows in the result set"
-		    
 		    v_arr = []
 		    p_arr = []
 		    play_hash = {}
@@ -37,19 +20,12 @@ def sql_querie(num, vs, play)
 		    v_rows.times do
 		        v_arr.push(vs.fetch_row.join("\s"))
 		    end
-		    puts v_arr
+		    #puts v_arr
 
 		    p_rows.times do
 		        p_arr.push(play.fetch_row.join("\s"))
 		    end  
-		    puts p_arr
-
-		    counter = 0
-		    v_arr.length.times do
-		    	play_hash[v_arr[counter]] = p_arr[counter]
-		    	counter +=1
-		    end
-		    puts play_hash
+		    #puts p_arr
 		    
 		rescue Mysql::Error => e
 		    puts e.errno
@@ -58,12 +34,16 @@ def sql_querie(num, vs, play)
 		ensure
 		    con.close if con
 		end
+		counter = 0
+		v_arr.length.times do
+		  	play_hash[v_arr[counter].to_i] = p_arr[counter]
+		   	counter +=1
+		end
 	else
 		puts "no data"
-
 	end
+	return play_hash
 end
 
-sql_querie(1, 'vslh', 'play')
-#sql_querie(1, 'vsrh')
-#sql_querie(1, 'play')
+
+puts sql_query(1, 'vslh', 'play')
--- play_mechanics_test.rb
+++ play_mechanics_test.rb
@@ -1,6 +1,61 @@
+#THIS FILE IS DEPRECATED - ALL MECHANICS MOVED TO play_mechanics_test.rb
+
+#batter info pulled from database
+require 'mysql'
+
+batter = "bloomquist_willie"
+pitcher = "bell_heath"
+
+def sql_query(player, vsHand, play)
+	if player != ''
+		begin
+		    con = Mysql.new 'localhost', 'root', 'waffles!', 'jLeague'
+
+		    vs = con.query("SELECT #{vsHand} FROM #{player}")
+		    v_rows = vs.num_rows
+
+		    play = con.query("SELECT #{play} FROM #{player}")
+		    p_rows = play.num_rows
+		    
+		    v_arr = []
+		    p_arr = []
+		    play_hash = {}
+
+		    v_rows.times do
+		        v_arr.push(vs.fetch_row.join("\s"))
+		    end
+		    #puts v_arr
+
+		    p_rows.times do
+		        p_arr.push(play.fetch_row.join("\s"))
+		    end  
+		    #puts p_arr
+		    
+		rescue Mysql::Error => e
+		    puts e.errno
+		    puts e.error
+		    
+		ensure
+		    con.close if con
+		end
+		counter = 0
+		v_arr.length.times do
+		  	play_hash[v_arr[counter].to_i] = p_arr[counter].upcase
+		   	counter +=1
+		end
+	else
+		puts "no data"
+	end
+	return play_hash
+end
+
+
+#puts sql_query(1, 'vslh', 'play')
+
+
 #placeholder values. Eventually these will be pulled from the databse
-batter_hash = {10 => "!", 67 => "E", 82 => "L_SS", 97 => "PARK", 128 => "1B_GCF", 129 => "3B_LCF", 226 => "HG_SS", 290 => "HG_2B", 322 => "HG_3B", 326 => "2B_LCF", 389 => "1B_LLF", 452 => "1B_LRF", 456 => "RG_3B", 457 => "HF_LF", 462 => "HB", 480 => "BB", 482 => "3B_CFW", 486 => "2B_LFL", 491 => "2B_LFW", 496 => "RG_SS", 498 => "HF_CF", 499 => "P_SS"}
-pitcher_hash = {514 => "PARK", 539 => "WP_PB", 564 => "OFR", 615 => "IFR", 616 => "HB", 624 => "SG_2B", 633 => "HF_CF", 651 => "1B_LCF", 658 => "HF_RF", 666=> "SG_SS", 673 => "HF_LF", 701 => "1B_GCF", 723 => "2B_RCF", 799 => "DF", 939 => "SO", 963 => "BB", 999 => "HG_2B"}
+#batter_hash = {10 => "!", 67 => "E", 82 => "L_SS", 97 => "PARK", 128 => "1B_GCF", 129 => "3B_LCF", 226 => "HG_SS", 290 => "HG_2B", 322 => "HG_3B", 326 => "2B_LCF", 389 => "1B_LLF", 452 => "1B_LRF", 456 => "RG_3B", 457 => "HF_LF", 462 => "HB", 480 => "BB", 482 => "3B_CFW", 486 => "2B_LFL", 491 => "2B_LFW", 496 => "RG_SS", 498 => "HF_CF", 499 => "P_SS"}
+#pitcher_hash = {514 => "PARK", 539 => "WP_PB", 564 => "OFR", 615 => "IFR", 616 => "HB", 624 => "SG_2B", 633 => "HF_CF", 651 => "1B_LCF", 658 => "HF_RF", 666=> "SG_SS", 673 => "HF_LF", 701 => "1B_GCF", 723 => "2B_RCF", 799 => "DF", 939 => "SO", 963 => "BB", 999 => "HG_2B"}
 
 #random dice roll for 000-999. This is the way the IBL game functions so I am going with it.
 dice_roll = rand(999)
@@ -11,9 +66,9 @@ play_hash = {}
 #if dice_roll <500 use batter_hash
 #if dice_roll >500 use pitcher_hash
 if dice_roll < 500
-	play_hash = batter_hash
+	play_hash = sql_query(batter, 'vslh', 'play_vslh')
 else
-	play_hash = pitcher_hash
+	play_hash = sql_query(pitcher, 'vslh', 'play_vslh')
 end
 
 #an empty array to store the play values
@@ -26,6 +81,7 @@ play_hash.each do |value, play|
 end
 
 #output the final play value and dice roll
+if dice_roll < 500 then puts "Batter" else puts "Pitcher" end
 puts dice_roll, play_array.first
 
 case play_array.first
